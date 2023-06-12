1419bb1b2dfb4c0e8576a05b2b6c3d68a57cf9b1
bruzen <k.w.robinson@gmail.com>
2023-06-12 15:00:16 -0400

    Try making interactive updating

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,49 @@
+import streamlit as st
 import altair as alt
 from vega_datasets import data
 
+# Load the iris dataset
 source = data.iris()
 
-base = alt.Chart(source)
+# Set up the Streamlit app
+st.title('Interactive Faceted Scatter Plot with Marginal Histograms')
 
+# Define the scales
 xscale = alt.Scale(domain=(4.0, 8.0))
 yscale = alt.Scale(domain=(1.9, 4.55))
 
-bar_args = {'opacity': .3, 'binSpacing': 0}
+# Define the bar arguments
+bar_args = {'opacity': 0.3, 'binSpacing': 0}
 
-points = base.mark_circle().encode(
-    alt.X('sepalLength').scale(xscale),
-    alt.Y('sepalWidth').scale(yscale),
-    color='species',
+# Create a slider to select the range of data
+data_range = st.slider('Data Range', 0, len(source), (0, len(source)))
+
+# Filter the data based on the selected range
+filtered_data = source[data_range[0]:data_range[1]]
+
+# Create the scatter plot
+points = alt.Chart(filtered_data).mark_circle().encode(
+    alt.X('sepalLength:Q', scale=xscale),
+    alt.Y('sepalWidth:Q', scale=yscale),
+    color='species:N'
 )
 
-top_hist = base.mark_bar(**bar_args).encode(
-    alt.X('sepalLength:Q')
-          # when using bins, the axis scale is set through
-          # the bin extent, so we do not specify the scale here
-          # (which would be ignored anyway)
-          .bin(maxbins=20, extent=xscale.domain)
-          .stack(None)
-          .title(''),
-    alt.Y('count()').stack(None).title(''),
-    alt.Color('species:N'),
+# Create the top histogram
+top_hist = alt.Chart(filtered_data).mark_bar(**bar_args).encode(
+    alt.X('sepalLength:Q', bin=alt.Bin(maxbins=20, extent=xscale.domain), stack=None, title=''),
+    alt.Y('count()', stack=None, title=''),
+    alt.Color('species:N')
 ).properties(height=60)
 
-right_hist = base.mark_bar(**bar_args).encode(
-    alt.Y('sepalWidth:Q')
-          .bin(maxbins=20, extent=yscale.domain)
-          .stack(None)
-          .title(''),
-    alt.X('count()').stack(None).title(''),
-    alt.Color('species:N'),
+# Create the right histogram
+right_hist = alt.Chart(filtered_data).mark_bar(**bar_args).encode(
+    alt.Y('sepalWidth:Q', bin=alt.Bin(maxbins=20, extent=yscale.domain), stack=None, title=''),
+    alt.X('count()', stack=None, title=''),
+    alt.Color('species:N')
 ).properties(width=60)
 
-top_hist & (points | right_hist)
+# Combine the plots using facet
+plot = top_hist & (points | right_hist)
+
+# Display the plot
+st.altair_chart(plot, use_container_width=True)
